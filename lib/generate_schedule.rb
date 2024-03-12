7df0ce6eacfe8a01c33f66922f1a8ae512b8df63
Apoorva Jain <91198172+apoorva-babbel@users.noreply.github.com>
2024-03-12 17:25:55 +0100

    Update OG headers

--- lib/generate_schedule.rb
+++ lib/generate_schedule.rb
@@ -4,13 +4,9 @@ require 'json'
 require 'uri'
 require 'net/http'
 require 'date'
+
 SCHEDULE_FILE = 'lib/schedule.json'
-
-user_team1 = pull_pg_schedule(schedule_id: "PGONDG5")
-user_team2 = pull_pg_schedule(schedule_id: "P0QYXI3")
-user_team3 = pull_pg_schedule(schedule_id: "PWAVTID")
-
-insert_schedule(team1: user_team1, team2: user_team2, team3: user_team3)
+VIEW_FILE = 'index.html'
 
 def pull_pg_schedule(schedule_id:)
   response = get_response(schedule_id: schedule_id)
@@ -55,3 +51,18 @@ def save_to_json(json_string)
   end
 end
 
+def update_og_headers
+  json_string = JSON.parse(File.read(SCHEDULE_FILE), { symbolize_names: true })
+  html_content = File.read(VIEW_FILE)
+  old_meta_tag = '<meta property="og:description" content="Lorem ipsum" />'
+  new_meta_tag = "<meta property='og:description' content='New Description #{Date.today.strftime('%V')}'>"
+  modified_content = html_content.gsub(old_meta_tag, new_meta_tag)
+  File.write(VIEW_FILE, modified_content)
+end
+
+user_team1 = pull_pg_schedule(schedule_id: "PGONDG5")
+user_team2 = pull_pg_schedule(schedule_id: "P0QYXI3")
+user_team3 = pull_pg_schedule(schedule_id: "PWAVTID")
+
+insert_schedule(team1: user_team1, team2: user_team2, team3: user_team3)
+update_og_headers
