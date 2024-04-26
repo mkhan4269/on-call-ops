1732ebdddc680732f9384c17b02aaabb79768984
Apoorva Jain <91198172+apoorva-babbel@users.noreply.github.com>
2024-04-26 18:18:33 +0200

    Generate metrics

--- lib/capture_log_entries.rb
+++ lib/capture_log_entries.rb
@@ -7,10 +7,11 @@ require 'date'
 
 LOG_ENTRIES_FILE = 'tmp/log_entries.json'
 LIMIT = 25
+# TODO: Add public holidays logic
+# public_holidays_2024 = []
 
 def pull_log_entries(offset: 0)
   response = get_response(offset: offset)
-  puts response[:more]
   insert_log_entries(log_entries: response[:log_entries])
 
   return unless response[:more] == true
@@ -23,6 +24,7 @@ def get_response(offset: 0)
   teams_param = teams_ids.map { |x| "team_ids[]=#{x}" }.join('&')
   since = "#{Date.today - 14}T00:00:00Z"
   until_today = "#{Date.today}T00:00:00Z"
+
   uri = URI("https://api.pagerduty.com/log_entries?#{teams_param}&since=#{since}&until=#{until_today}&include[]=incidents&limit=#{LIMIT}&offset=#{offset}")
   request = Net::HTTP::Get.new(uri)
   request['Authorization'] = "Token token=#{ENV['PD_TOKEN']}"
@@ -55,5 +57,61 @@ def clear_log_entries
   end
 end
 
+def generate_metrics
+  data = JSON.parse(File.read(LOG_ENTRIES_FILE), { symbolize_names: true })
+  grouped_by_team = data.group_by { |log_entry| log_entry[:teams][0][:summary] }
+  teams = grouped_by_team.keys
+
+  metrics = {}
+  teams.each do |team|
+    grouped_by_incident = grouped_by_team[team].group_by { |log_entry| log_entry[:incident][:incident_number] }
+    incidents = determine_work_hours_and_urgency(grouped_by_incident.map { |_k, v| v[0] })
+    on_work_hours_low_urgency = incidents.select { |_k, v| v[:in_work_hours] && v[:urgency] == 'low' }
+    on_work_hours_high_urgency = incidents.select { |_k, v| v[:in_work_hours] && v[:urgency] == 'high' }
+    off_work_hours_low_urgency = incidents.select { |_k, v| !v[:in_work_hours] && v[:urgency] == 'low' }
+    off_work_hours_high_urgency = incidents.select { |_k, v| !v[:in_work_hours] && v[:urgency] == 'high' }
+
+    metrics[team] = {
+      total_incidents: grouped_by_incident.keys.count,
+      incidents_on_work_hours: {
+        low_urgency: on_work_hours_low_urgency.count,
+        high_urgency: on_work_hours_high_urgency.count
+      },
+      incidents_off_work_hours: {
+        low_urgency: off_work_hours_low_urgency.count,
+        high_urgency: off_work_hours_high_urgency.count
+      }
+    }
+  end
+  puts metrics
+end
+
+def determine_work_hours_and_urgency(log_entries)
+  incidents_created_at = {}
+  log_entries.map do |entry|
+    time = DateTime.parse(entry[:incident][:created_at]) + (2.0 / 24.0)
+    incidents_created_at[entry[:incident][:incident_number]] = {
+      in_work_hours: on_work_hours?(time) && !in_weekend?(time),
+      urgency: entry[:incident][:urgency]
+    }
+  end
+  incidents_created_at
+end
+
+def on_work_hours?(time)
+  return true if time.hour >= 10 && time.hour < 18
+
+  return true if time.hour == 18 && time.minute <= 30
+
+  false
+end
+
+def in_weekend?(time)
+  time.saturday? || time.sunday?
+end
+
 clear_log_entries
+puts 'Pulling log entries'
 pull_log_entries
+puts 'Generating metrics'
+generate_metrics
